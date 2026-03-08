468a53a43e766c77c6c8c8496f517cc24273c678
miniBig <bghbmh@gmail.com>
2026-03-08 15:18:58 +0900

    형식변경

--- src/app/studio/members/[id]/notes/new/layout.tsx
+++ src/app/studio/members/[id]/notes/new/layout.tsx
@@ -1,16 +1,14 @@
 import BackHeader from '@/components/BackHeader'
 
 export default function NoteNewLayout({
-  children,
-  params,
+	children,
 }: {
-  children: React.ReactNode
-  params: { id: string }
+	children: React.ReactNode
 }) {
-  return (
-    <>
-      <BackHeader title="알림장 작성" backLabel="← 회원목록" />
-      <div className="p-5">{children}</div>
-    </>
-  )
+	return (
+		<>
+			<BackHeader title="알림장 작성" backLabel="← 회원목록" />
+			<div className="p-5">{children}</div>
+		</>
+	)
 }
